62ebc7a1152d0a415b4d83e0aa613154bf7b42b5
DeVoytinc <egor_pomidor-1998@mail.ru>
2023-11-11 18:39:04 +0600

    Initial Commit

---
+++ SwiftLabEffective/SwiftLabEffectiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLabEffectiveApp.swift
+//  SwiftLabEffective
+//
+//  Created by  user on 11.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLabEffectiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
